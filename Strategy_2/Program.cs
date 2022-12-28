96a90a1a1e5239341ac5021d121fd009aab480d9
sk-whitedad <senkos@mail.ru>
2022-12-28 15:24:39 +0300

    Strategy_2

---
+++ Strategy_2/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Strategy_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car auto = new Car(4, "Volvo", new PetrolMove());
+            auto.Move();
+            auto.Movable = new ElectricMove();
+            auto.Move();
+            Console.ReadLine();
+        }
+    }
+    interface IMovable
+    {
+        void Move();
+    }
+
+    class PetrolMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на бензине");
+        }
+    }
+
+    class ElectricMove : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Перемещение на электричестве");
+        }
+    }
+    class Car
+    {
+        protected int passengers; // кол-во пассажиров
+        protected string model; // модель автомобиля
+
+        public Car(int num, string model, IMovable mov)
+        {
+            this.passengers = num;
+            this.model = model;
+            Movable = mov;
+        }
+        public IMovable Movable { private get; set; }
+        public void Move()
+        {
+            Movable.Move();
+        }
+    }
+}
